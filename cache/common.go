89fb6767404e41292d22318fd4157987831e9331
jax <xdxduser@hotmail.com>
2020-03-11 14:19:10 -0700

    updates
    Signed-off-by: jax <xdxduser@hotmail.com>

--- cache/common.go
+++ cache/common.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	CacheEmptyError      = errors.New("for cache empty")
 	CacheHandlerNotFound = errors.New("not found cache handler registered")
+	CacheLockFound       = errors.New("cache lock is existent")
 )
